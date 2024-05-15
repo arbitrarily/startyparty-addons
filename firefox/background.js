24e9b319e68191230418ea4eaea638bf90bc5478
Marko Bajlovic <mbajlovic@gmail.com>
2024-05-15 19:36:44 -0400

    updates

--- firefox/background.js
+++ firefox/background.js
@@ -1,25 +1,14 @@
-let isNewSession = true;
 const homepageUrl = "https://startyparty.dev";
-
-chrome.tabs.onCreated.addListener((tab) => {
-  if (
-    isNewSession &&
-    (tab.url === "about:blank" || tab.url === "about:newtab")
-  ) {
-    chrome.tabs.update(tab.id, { url: homepageUrl });
-  }
-});
-
-chrome.windows.onCreated.addListener(() => {
-  if (isNewSession) {
-    isNewSession = false;
-  }
-});
-
-chrome.runtime.onStartup.addListener(() => {
-  isNewSession = true;
-});
 
 chrome.browserAction.onClicked.addListener((tab) => {
   chrome.tabs.update(tab.id, { url: homepageUrl });
+});
+
+browser.windows.onCreated.addListener(async (window) => {
+  if (window.type === "normal") {
+    const tabs = await browser.tabs.query({ windowId: window.id });
+    if (tabs.length === 1 && tabs[0].url === "about:blank") {
+      await browser.tabs.update(tabs[0].id, { url: homepageUrl });
+    }
+  }
 });
