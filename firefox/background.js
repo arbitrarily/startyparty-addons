0f0f586f301a858a00eaab3aa812509e7ddf984a
Marko Bajlovic <mbajlovic@gmail.com>
2024-05-16 17:50:51 -0400

    firefox addon updates

--- firefox/background.js
+++ firefox/background.js
@@ -1,14 +1,23 @@
 const homepageUrl = "https://startyparty.dev";
 
+// Click Icon
 chrome.browserAction.onClicked.addListener((tab) => {
   chrome.tabs.update(tab.id, { url: homepageUrl });
 });
 
+// Open New Window
 browser.windows.onCreated.addListener(async (window) => {
-  if (window.type === "normal") {
-    const tabs = await browser.tabs.query({ windowId: window.id });
-    if (tabs.length === 1 && tabs[0].url === "about:blank") {
-      await browser.tabs.update(tabs[0].id, { url: homepageUrl });
-    }
+  if (window.type !== "normal") return;
+
+  const w = await browser.windows.get(window.id, { populate: true });
+  const tabs = await browser.tabs.query({ windowId: w.id });
+
+  if (
+    tabs.length === 1 &&
+    tabs[0].url === "about:blank" &&
+    tabs[0].status === "complete" &&
+    tabs[0].title === "New Tab"
+  ) {
+    await browser.tabs.update(tabs[0].id, { url: homepageUrl });
   }
 });
