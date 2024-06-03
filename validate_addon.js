d337b303e04121edf67f7cb93e27fcbe442a7c2a
rooki <rooki@rooki.xyz>
2024-06-03 14:09:37 +0200

    Added a test script

---
+++ validate_addon.js
@@ -0,0 +1,25 @@
+import webExt from "web-ext";
+
+async function validate() {
+  const result = await webExt.cmd.lint({
+    sourceDir: "firefox",
+    output: "json",
+  }, {
+    shouldExitProgram: false,
+  });
+  const { count } = result;
+
+  if (count > 0) {
+    console.error(
+      "Errors found in the extension. Please fix them before proceeding."
+    );
+    console.error("===============================================");
+    console.error(JSON.stringify(result, null, 2));
+    console.error("===============================================");
+    console.error("Exiting the process...");
+    process.exit(1);
+  }
+  process.exit(0);
+}
+
+validate();
